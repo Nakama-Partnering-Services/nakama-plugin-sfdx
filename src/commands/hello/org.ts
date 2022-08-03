f42dec0fdb8b223fee86ba0df044435d97c5e6e5
FIRST_NAME LAST_NAME <gabriel.serrano.salas@outlook.com>
2022-08-03 13:49:26 +0200

    Formatted files

--- src/commands/hello/org.ts
+++ src/commands/hello/org.ts
@@ -17,77 +17,77 @@ Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('plugin-coverage', 'org');
 
 export default class Org extends SfdxCommand {
-  public static description = messages.getMessage('commandDescription');
+	public static description = messages.getMessage('commandDescription');
 
-  public static examples = messages.getMessage('examples').split(os.EOL);
+	public static examples = messages.getMessage('examples').split(os.EOL);
 
-  public static args = [{ name: 'file' }];
+	public static args = [{ name: 'file' }];
 
-  protected static flagsConfig = {
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({
-      char: 'n',
-      description: messages.getMessage('nameFlagDescription'),
-    }),
-    force: flags.boolean({
-      char: 'f',
-      description: messages.getMessage('forceFlagDescription'),
-    }),
-  };
+	protected static flagsConfig = {
+		// flag with a value (-n, --name=VALUE)
+		name: flags.string({
+			char: 'n',
+			description: messages.getMessage('nameFlagDescription')
+		}),
+		force: flags.boolean({
+			char: 'f',
+			description: messages.getMessage('forceFlagDescription')
+		})
+	};
 
-  // Comment this out if your command does not require an org username
-  protected static requiresUsername = true;
+	// Comment this out if your command does not require an org username
+	protected static requiresUsername = true;
 
-  // Comment this out if your command does not support a hub org username
-  protected static supportsDevhubUsername = true;
+	// Comment this out if your command does not support a hub org username
+	protected static supportsDevhubUsername = true;
 
-  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-  protected static requiresProject = false;
+	// Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+	protected static requiresProject = false;
 
-  public async run(): Promise<AnyJson> {
-    const name = (this.flags.name || 'world') as string;
+	public async run(): Promise<AnyJson> {
+		const name = (this.flags.name || 'world') as string;
 
-    // this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
-    const conn = this.org.getConnection();
-    const query = 'Select Name, TrialExpirationDate from Organization';
+		// this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
+		const conn = this.org.getConnection();
+		const query = 'Select Name, TrialExpirationDate from Organization';
 
-    // The type we are querying for
-    interface Organization {
-      Name: string;
-      TrialExpirationDate: string;
-    }
+		// The type we are querying for
+		interface Organization {
+			Name: string;
+			TrialExpirationDate: string;
+		}
 
-    // Query the org
-    const result = await conn.query<Organization>(query);
+		// Query the org
+		const result = await conn.query<Organization>(query);
 
-    // Organization will always return one result, but this is an example of throwing an error
-    // The output and --json will automatically be handled for you.
-    if (!result.records || result.records.length <= 0) {
-      throw new SfError(messages.getMessage('errorNoOrgResults', [this.org.getOrgId()]));
-    }
+		// Organization will always return one result, but this is an example of throwing an error
+		// The output and --json will automatically be handled for you.
+		if (!result.records || result.records.length <= 0) {
+			throw new SfError(messages.getMessage('errorNoOrgResults', [this.org.getOrgId()]));
+		}
 
-    // Organization always only returns one result
-    const orgName = result.records[0].Name;
-    const trialExpirationDate = result.records[0].TrialExpirationDate;
+		// Organization always only returns one result
+		const orgName = result.records[0].Name;
+		const trialExpirationDate = result.records[0].TrialExpirationDate;
 
-    let outputString = `Hello ${name}! This is org: ${orgName}`;
-    if (trialExpirationDate) {
-      const date = new Date(trialExpirationDate).toDateString();
-      outputString = `${outputString} and I will be around until ${date}!`;
-    }
-    this.ux.log(outputString);
+		let outputString = `Hello ${name}! This is org: ${orgName}`;
+		if (trialExpirationDate) {
+			const date = new Date(trialExpirationDate).toDateString();
+			outputString = `${outputString} and I will be around until ${date}!`;
+		}
+		this.ux.log(outputString);
 
-    // this.hubOrg is NOT guaranteed because supportsHubOrgUsername=true, as opposed to requiresHubOrgUsername.
-    if (this.hubOrg) {
-      const hubOrgId = this.hubOrg.getOrgId();
-      this.ux.log(`My hub org id is: ${hubOrgId}`);
-    }
+		// this.hubOrg is NOT guaranteed because supportsHubOrgUsername=true, as opposed to requiresHubOrgUsername.
+		if (this.hubOrg) {
+			const hubOrgId = this.hubOrg.getOrgId();
+			this.ux.log(`My hub org id is: ${hubOrgId}`);
+		}
 
-    if (this.flags.force && this.args.file) {
-      this.ux.log(`You input --force and a file: ${this.args.file as string}`);
-    }
+		if (this.flags.force && this.args.file) {
+			this.ux.log(`You input --force and a file: ${this.args.file as string}`);
+		}
 
-    // Return an object to be displayed with --json
-    return { orgId: this.org.getOrgId(), outputString };
-  }
+		// Return an object to be displayed with --json
+		return { orgId: this.org.getOrgId(), outputString };
+	}
 }
