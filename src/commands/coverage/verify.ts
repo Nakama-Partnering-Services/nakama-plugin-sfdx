00f6b68361f90de4804aebe2915ce21bcc95a33f
FIRST_NAME LAST_NAME <gabriel.serrano.salas@outlook.com>
2022-08-03 14:46:53 +0200

    Improved error handling + absolute paths

--- src/commands/coverage/verify.ts
+++ src/commands/coverage/verify.ts
@@ -3,11 +3,13 @@ import { flags, SfdxCommand } from '@salesforce/command';
 import { Messages, SfError } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
 
+import { resolve as resolvePath } from 'path';
+
 Messages.importMessagesDirectory(__dirname);
 
 const messages = Messages.loadMessages('plugin-coverage', 'verify');
 
-export default class Org extends SfdxCommand {
+export default class Verify extends SfdxCommand {
 	public static description = messages.getMessage('commandDescription');
 
 	public static examples = messages.getMessage('examples').split(os.EOL);
@@ -17,42 +19,53 @@ export default class Org extends SfdxCommand {
 	protected static flagsConfig = {
 		path: flags.string({
 			char: 'p',
-			description: messages.getMessage('pathToFileFlagDescription'),
+			description: messages.getMessage('pathToFileFlagDescription')
 		}),
 		requiredcoverage: flags.number({
 			char: 'r',
-			description: messages.getMessage('requiredCoverageFlagDescription'),
+			description: messages.getMessage('requiredCoverageFlagDescription')
 		}),
 		classes: flags.string({
 			char: 'c',
-			description: messages.getMessage('classesToCheckFlagDescription'),
-		}),
+			description: messages.getMessage('classesToCheckFlagDescription')
+		})
 	};
 
 	public async run(): Promise<AnyJson> {
 		const requiredCoverage = (this.flags.requiredcoverage || 75) as number;
 
-		const deploymentResult = require(this.flags.path);
+		let deploymentResult;
+		try {
+			deploymentResult = require(resolvePath(this.flags.path));
+		} catch (e) {
+			throw new SfError(messages.getMessage('errorNoCoverageFileFound', [this.flags.path]));
+		}
 
-		const apexTestResults = deploymentResult.result.details.runTestResult.codeCoverage.reduce(
-			(result, detail) => ({
-				...result,
-				[detail.name]: detail
-			}),
-			{}
-		);
+		let apexTestResults;
+		try {
+			apexTestResults = deploymentResult.result.details.runTestResult.codeCoverage.reduce(
+				(result, detail) => ({
+					...result,
+					[detail.name]: detail
+				}),
+				{}
+			);
+		} catch (e) {
+			throw new SfError(messages.getMessage('errorImproperCoverageFileFormat'));
+		}
 
-		const classesToCheck = this.flags.classes.split(',');
+		const classesToCheck = this.flags.classes?.split(',');
 
 		if (!classesToCheck) {
-			const msg = 'There are no apex test classes to check coverage.';
-			this.ux.log(msg);
-			return;
-			// process.exit(0);
+			throw new SfError(messages.getMessage('errorNoClassesToCheck'));
 		}
 
 		const classesWithCoverage = classesToCheck.map((testClassName) => {
 			const testClassDetails = apexTestResults[testClassName];
+
+			if (!testClassDetails) {
+				throw new SfError(messages.getMessage('noClassFound', [testClassName]));
+			}
 
 			const totalLines = testClassDetails.numLocations;
 			const linesNotCovered = testClassDetails.numLocationsNotCovered;
@@ -64,6 +77,7 @@ export default class Org extends SfdxCommand {
 			};
 		});
 
+		this.ux.log(messages.getMessage('listOfAnalyzedClasses'));
 		this.ux.log(classesWithCoverage.map((coverage) => `${coverage.class}: ${coverage.percentage}%`).join('\n'));
 
 		const classesWithoutEnoughCoverage = classesWithCoverage
@@ -71,14 +85,16 @@ export default class Org extends SfdxCommand {
 			.map((item) => item.class);
 
 		if (classesWithoutEnoughCoverage.length > 0) {
-			throw new SfError(messages.getMessage('errorInsufficientCoverageForClasses', [requiredCoverage, classesWithoutEnoughCoverage.join(
-				', '
-			)]));
+			throw new SfError(
+				messages.getMessage('errorInsufficientCoverageForClasses', [
+					requiredCoverage,
+					classesWithoutEnoughCoverage.join(', ')
+				])
+			);
 		}
 
-		this.ux.log('No classes with insufficient coverage found');
+		this.ux.log(messages.getMessage('noClassesWithInsufficientCoverage'));
 
-		// Return an object to be displayed with --json
-		return; // { orgId: this.org.getOrgId(), outputString };
+		return { success: true, classesWithCoverage };
 	}
 }
