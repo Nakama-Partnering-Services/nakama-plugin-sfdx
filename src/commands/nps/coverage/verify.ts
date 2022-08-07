fe2cfc3c2803c147ae862cbe6eb045ed65f763e7
FIRST_NAME LAST_NAME <gabriel.serrano.salas@outlook.com>
2022-08-08 00:24:07 +0200

    refactor: Improved readability and safety

--- src/commands/nps/coverage/verify.ts
+++ src/commands/nps/coverage/verify.ts
@@ -17,36 +17,71 @@ export default class Verify extends SfdxCommand {
 	public static args = [{ name: 'file' }];
 
 	protected static flagsConfig = {
-		path: flags.string({
+		path: flags.filepath({
 			char: 'p',
 			description: messages.getMessage('pathToFileFlagDescription'),
 			required: true
 		}),
-		'required-coverage': flags.number({
+		'required-coverage': flags.integer({
 			char: 'r',
 			description: messages.getMessage('requiredCoverageFlagDescription'),
-			default: 75
+			default: 75,
+			min: 75,
+			max: 100
 		}),
-		classes: flags.string({
+		classes: flags.array({
 			char: 'c',
 			description: messages.getMessage('classesToCheckFlagDescription'),
-			required: true
+			required: true,
+			delimiter: ','
 		})
 	};
 
 	public async run(): Promise<AnyJson> {
-		const requiredCoverage = (this.flags['required-coverage'] || 75) as number;
+		const coverage = this.flags['required-coverage'];
 
-		let deploymentResult;
-		try {
-			deploymentResult = require(resolvePath(this.flags.path));
-		} catch (e) {
-			throw new SfError(messages.getMessage('errorNoCoverageFileFound', [this.flags.path]));
+		const allClassesWithCoverage = this.allClassesWithCoverage;
+
+		const classesNotCovered = allClassesWithCoverage
+			.filter((item) => item.percentage < coverage)
+			.map((item) => item.class);
+
+		if (classesNotCovered.length > 0) {
+			throw new SfError(messages.getMessage('errorClassesNotCovered', [coverage, classesNotCovered.join(', ')]));
 		}
 
-		let apexTestResults;
+		this.ux.log(messages.getMessage('noClassesWithInsufficientCoverage'));
+
+		return { success: true, allClassesWithCoverage };
+	}
+
+	get allClassesWithCoverage() {
+		const result = this.flags.classes.map((className) => {
+			const classDetails = this.apexTestResults[className];
+
+			if (!classDetails) {
+				throw new SfError(messages.getMessage('errorNoClassFound', [className]), 'NoClassFound');
+			}
+
+			const totalLines = classDetails.numLocations;
+			const linesNotCovered = classDetails.numLocationsNotCovered;
+			const linesCovered = totalLines - linesNotCovered;
+			const percentCoverage = (linesCovered / totalLines) * 100;
+			return {
+				class: className,
+				percentage: percentCoverage
+			};
+		});
+
+		this.ux.log(messages.getMessage('listOfAnalyzedClasses'));
+		this.ux.log(result.map((coverage) => `${coverage.class}: ${coverage.percentage}%`).join('\n'));
+
+		return result;
+	}
+
+	get apexTestResults() {
 		try {
-			apexTestResults = deploymentResult.result.details.runTestResult.codeCoverage.reduce(
+			return this.deploymentResult.result.details.runTestResult.codeCoverage.reduce(
 				(result, detail) => ({
 					...result,
 					[detail.name]: detail
@@ -54,50 +89,18 @@ export default class Verify extends SfdxCommand {
 				{}
 			);
 		} catch (e) {
-			throw new SfError(messages.getMessage('errorImproperCoverageFileFormat'));
+			throw new SfError(messages.getMessage('errorImproperCoverageFileFormat'), 'ImproperCoverageFileFormat');
 		}
+	}
 
-		const classesToCheck = this.flags.classes?.split(',');
-
-		if (!classesToCheck) {
-			throw new SfError(messages.getMessage('errorNoClassesToCheck'));
-		}
-
-		const classesWithCoverage = classesToCheck.map((testClassName) => {
-			const testClassDetails = apexTestResults[testClassName];
-
-			if (!testClassDetails) {
-				throw new SfError(messages.getMessage('noClassFound', [testClassName]));
-			}
-
-			const totalLines = testClassDetails.numLocations;
-			const linesNotCovered = testClassDetails.numLocationsNotCovered;
-			const linesCovered = totalLines - linesNotCovered;
-			const percentCoverage = (linesCovered / totalLines) * 100;
-			return {
-				class: testClassName,
-				percentage: percentCoverage
-			};
-		});
-
-		this.ux.log(messages.getMessage('listOfAnalyzedClasses'));
-		this.ux.log(classesWithCoverage.map((coverage) => `${coverage.class}: ${coverage.percentage}%`).join('\n'));
-
-		const classesWithoutEnoughCoverage = classesWithCoverage
-			.filter((item) => item.percentage < requiredCoverage)
-			.map((item) => item.class);
-
-		if (classesWithoutEnoughCoverage.length > 0) {
+	get deploymentResult() {
+		try {
+			return require(resolvePath(this.flags.path));
+		} catch (e) {
 			throw new SfError(
-				messages.getMessage('errorInsufficientCoverageForClasses', [
-					requiredCoverage,
-					classesWithoutEnoughCoverage.join(', ')
-				])
+				messages.getMessage('errorNoCoverageFileFound', [this.flags.path]),
+				'NoCoverageFileFound'
 			);
 		}
-
-		this.ux.log(messages.getMessage('noClassesWithInsufficientCoverage'));
-
-		return { success: true, classesWithCoverage };
 	}
 }
